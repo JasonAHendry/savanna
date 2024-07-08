c690310d879b87ee99751397e608da9d9b25753a
JasonAHendry <jason.ahendry@gmail.com>
2024-07-08 16:13:49 +0200

    Updated example data and including pipeline flag.

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -1,7 +1,9 @@
 #Example of how to run `savanna analyse` from the command line
 
 savanna analyse \
--e 0000-00-00_example \
--f example_data/pf_set1/fastq_pass \
--m example_data/pf_set1/metadata/sample_info-mvp.csv \
--b example_data/pf_set1/metadata/nomadsMVP.amplicons.bed
+-e 0000-00-00_expt1 \
+-f example_data/expt1/fastq_pass \
+-m example_data/expt1/metadata/sample_info.csv \
+-r example_data/expt1/metadata/nomads8.amplicons.bed \
+--pipeline plasmo 
+
